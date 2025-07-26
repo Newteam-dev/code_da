47e7920e3f63ba073216990d59f7c341e744d41f
New Team Dev <newteam.media@hotmail.com>
2025-07-26 15:11:14 +0700

    s

--- src/components/admin/header.tsx
+++ src/components/admin/header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AdminHeader = () => {
   return (
     <header className='bg-white w-full shadow-md flex p-4 relative z-50'>
-        <div className='logo w-1/5'>Ngocnv34</div>
+        <div className='logo w-1/5'>admin</div>
         <div className='right-header w-4/5 flex justify-between'>
             <form>
                 <input className='border rounded-md w-[350px] px-2 py-1' type='text' placeholder='Tìm kiếm'/>
